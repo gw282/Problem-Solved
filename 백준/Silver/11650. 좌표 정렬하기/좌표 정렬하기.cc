1abc884a7c815192f73a3c1e845597bf369c85d8
Gi-Woong Lee <80272318+LeeGiWoong@users.noreply.github.com>
2023-08-08 17:51:53 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 48 ms, Memory: 3692 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,37 @@
+/*
+[2023-08-08 11650 좌표 정렬하기]
+stable_sort: 기존 순서를 보장해줌 (sort와의 차이점)
+make_pair: pair를 만들어주는 함수
+ex) pair<int,int> num = make_pair(1,2);
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool cmp(pair<int,int> a, pair<int,int> b) {
+    if(a.first != b.first) return a.first < b.first;
+    return a.second < b.second; 
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    int i1,i2;
+    cin >> n;
+    vector<pair<int,int>> v;
+    while(n--) {
+        cin >> i1 >> i2;
+        pair<int,int> info = make_pair(i1,i2);
+        v.push_back(info);
+    }
+
+    stable_sort(v.begin(), v.end(),cmp);
+
+    for(int i=0;i<v.size();i++) {
+        cout << v[i].first << ' ' << v[i].second << '\n';
+    }
+}
