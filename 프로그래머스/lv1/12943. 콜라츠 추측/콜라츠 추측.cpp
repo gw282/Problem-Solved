f2200583a479455d75edd10d0bec5a125d981236
gw282 <80272318+gw282@users.noreply.github.com>
2023-09-13 22:31:34 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    int cnt = 0;
+    
+    while(num != 1) {
+        if(cnt >= 500) {
+            return -1;
+        }
+        if(num % 2 == 0) num /= 2;
+        else if(num % 2 == 1) num = num * 3 + 1;
+        cnt++;
+    }
+    
+    answer = cnt;
+    return answer;
+}
