905cba70008368aaf502203df1b2b5cb14168c92
gw282 <80272318+gw282@users.noreply.github.com>
2023-09-17 17:10:51 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,20 @@
+/*
+[2023-09-16 29751 삼각형]
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    float w, h, n;
+    cin >> w >> h;
+    n = w*h/2;
+    
+    cout << fixed;
+    cout.precision(1);
+    cout << n;
+}
