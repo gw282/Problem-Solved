6a90357c5926be8ca6758a16efbe7d17f2c0b6b0
gw282 <80272318+gw282@users.noreply.github.com>
2023-09-19 22:13:27 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "Yes";
+    
+    int c1=0, c2=0;
+    
+    for(int i=0;i<goal.size();i++) {
+        if(goal[i] == cards1[c1]) c1++;
+        else if(goal[i] == cards2[c2]) c2++;
+        else return "No";
+    }
+    return answer;
+}
