7ca2214ac729b088d846450eb359303b60708b21
gw282 <80272318+gw282@users.noreply.github.com>
2023-09-20 22:03:42 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    
+    sort(score.begin(),score.end(),greater<>());
+    
+    for(int i=m-1;i<score.size();i=i+m) {
+        if(score[i] > k) score[i] = k;
+        answer += score[i] * m;
+    }
+    return answer;
+}
