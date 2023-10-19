8913a5b985191bf3f2e8313cb96d8b63aebf7f38
gw282 <80272318+gw282@users.noreply.github.com>
2023-10-19 10:45:00 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int r1, s;
+
+    cin >> r1 >> s;
+
+    cout << s*2-r1;
+    
+
+}
