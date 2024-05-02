68f4d4837dfa0fd6864da64b734111386ca2a23a
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-05-02 23:07:54 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -4,53 +4,24 @@
 using namespace std;
 
 vector<int> solution(vector<string> wallpaper) {
-    vector<int> answer;
-    bool min_x =false, min_y=false, max_x=false, max_y=false;
-    
-    for(int i=0;i<wallpaper.size();i++) {
-        for(int j=0;j<wallpaper[0].length();j++) {
-            if(wallpaper[i][j] == '#') {
-                answer.push_back(i); 
-                min_x = true;
-                break;
-            }
-        }
-        if(min_x == true) break;
-    }
-    
+    vector<int> answer{100,100,-100,-100};
 
-    for(int j=0;j<wallpaper[0].length();j++) {
-        for(int i=0;i<wallpaper.size();i++) {
-            if(wallpaper[i][j] == '#') {
-                answer.push_back(j);
-                min_y= true;
-                break;
+    const int n = wallpaper.size();
+    const int m = wallpaper[0].size();
+
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j < m ; j++)
+        {
+            if(wallpaper[i][j] == '#')
+            {
+                answer[0] = min(answer[0],i);
+                answer[1] = min(answer[1],j);
+                answer[2] = max(answer[2],i+1);
+                answer[3] = max(answer[3],j+1);
             }
         }
-        if(min_y== true) break;
-    }
-    
-    
-    for(int i=wallpaper.size()-1;i>=0;i--) {
-        for(int j=wallpaper[0].length()-1;j>=0;j--) {
-            if(wallpaper[i][j] == '#') {
-                answer.push_back(i+1); 
-                max_x = true;
-                break;
-            }
-        }
-        if(max_x == true) break;
-    }
-    
-    for(int j=wallpaper[0].length()-1;j>=0;j--) {
-        for(int i=wallpaper.size()-1;i>=0;i--) {
-            if(wallpaper[i][j] == '#') {
-                answer.push_back(j+1);
-                max_y= true;
-                break;
-            }
-        }
-        if(max_y== true) break;
     }
 
     return answer;
