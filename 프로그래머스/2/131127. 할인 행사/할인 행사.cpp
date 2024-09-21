49f543ee13368b3bf949ce8545a8e40850bd17cd
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2024-09-21 17:20:26 +0900

    [level 2] Title: 할인 행사, Time: 78.61 ms, Memory: 8.79 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -8,7 +8,7 @@ int solution(vector<string> want, vector<int> number, vector<string> discount) {
     int answer = 0;
 
     // i부터 시작하는 10일 간의 항목을 검사
-    for (int i = 0; i <= discount.size() - 10; i++) {
+    for (int i=0; i<=discount.size()-10;i++) {
         
         map<string, int> itemMap;
         
@@ -18,7 +18,7 @@ int solution(vector<string> want, vector<int> number, vector<string> discount) {
         }
         
         // i부터 시작하는 10일간의 할인 항목을 itemMap에 기록
-        for (int j = i; j < i + 10; j++) {
+        for (int j=i;j<i+10;j++) {
             if (itemMap.find(discount[j]) != itemMap.end()) {
                 itemMap[discount[j]]++;
             }
@@ -26,7 +26,7 @@ int solution(vector<string> want, vector<int> number, vector<string> discount) {
         
         // 모든 want 항목의 개수가 충족되는지 확인
         bool match = true;
-        for (int j = 0; j < want.size(); j++) {
+        for (int j=0;j<want.size();j++) {
             if (itemMap[want[j]] != number[j]) {
                 match = false;
                 break;
