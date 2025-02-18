5895104eef86dbaa987d9ac72be7d90ddd88c909
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2025-02-18 18:11:46 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.92 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -10,15 +10,14 @@ vector<int> solution(vector<int> fees, vector<string> records) {
     int standard_fee = fees[1];
     int unit_time = fees[2];
     int unit_fee = fees[3];
-    int time_cnt;
     
     map<int, vector<int>> rec;
     
-    for(int i=0;i<records.size();i++) {
+    for (int i = 0; i < records.size(); i++) {
         int time, num;
         string record = records[i];
-        time = stoi(record.substr(0,2)) * 60 + stoi(record.substr(3,2));
-        num = stoi(record.substr(6,4));
+        time = stoi(record.substr(0, 2)) * 60 + stoi(record.substr(3, 2));
+        num = stoi(record.substr(6, 4));
         
         rec[num].push_back(time);
     }
@@ -29,22 +28,24 @@ vector<int> solution(vector<int> fees, vector<string> records) {
         int num = ord.first;
         vector<int> times = ord.second;
         
-        if(times.size() % 2 == 1) {
-            sum = sum + 1439 -times[times.size()-1];
+        // 마지막 출차 시간 처리
+        if (times.size() % 2 == 1) {
+            sum += 1439 - times[times.size() - 1];  // 자정까지 남은 시간
         }
-        for(int i=0;i<times.size()/2;i++) {
-            sum = sum + times[i*2+1] - times[i*2];
+        
+        // 입차와 출차 시간의 차이 계산
+        for (int i = 0; i < times.size() / 2; i++) {
+            sum += times[i * 2 + 1] - times[i * 2];
         }
-        if(sum - standard_time <= 0) time_cnt = 0;
-        else if((sum - standard_time) % unit_time != 0) {
-            time_cnt = (sum - standard_time) / unit_time + 1;
-        } else {
-            time_cnt = (sum - standard_time) / unit_time;
-        }
+        
+        // 기본 시간보다 더 오래 주차한 경우 요금 계산
+        int extra_time = sum - standard_time;
+        int time_cnt = (extra_time > 0) ? (extra_time + unit_time - 1) / unit_time : 0;
+        
         fee = standard_fee + time_cnt * unit_fee;
         
         answer.push_back(fee);
-        
     }
+    
     return answer;
 }
