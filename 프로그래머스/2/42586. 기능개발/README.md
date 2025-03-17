63ed2dc5e65ed44108bcbff55beb5492de8a9475
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2025-03-17 20:46:32 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 4.17 MB, 시간: 0.03 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 01일 17:16:34
+2025년 03월 17일 20:46:30
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,36 +1,32 @@
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    vector<int> days;  // 각 기능을 완성하는 데 필요한 날짜를 저장하는 벡터
-    
-    for(int i = 0; i < speeds.size(); i++) {
+    queue<int> q;
+
+    // 각 기능이 완료되기까지 필요한 일수를 계산하여 queue에 저장
+    for (int i = 0; i < progresses.size(); i++) {
         int remainingProgress = 100 - progresses[i];
         int day = (remainingProgress + speeds[i] - 1) / speeds[i]; // 올림 계산
-        days.push_back(day);
+        q.push(day);
     }
 
-    int count = 0; // 배포할 기능 수
-    int maxDays = days[0]; // 첫 번째 기능의 최대 날짜로 초기화
+    while (!q.empty()) {
+        int count = 1; // 배포할 기능 개수
+        int maxDay = q.front(); // 첫 번째 기능의 배포 기준일
+        q.pop();
 
-    for(int i = 0; i < days.size(); i++) {
-        if (days[i] <= maxDays) {
-            count++; // 현재 maxDays 내에서 배포 가능한 기능 수 증가
-        } else {
-            if (count > 0) {
-                answer.push_back(count); // 배포할 기능 수가 있으면 answer에 추가
-            }
-            count = 1; // 새로운 기능의 경우 카운트 초기화
-            maxDays = days[i]; // maxDays 업데이트
+        // 뒤에 있는 기능이 앞 기능과 함께 배포될 수 있는지 확인
+        while (!q.empty() && q.front() <= maxDay) {
+            count++;
+            q.pop();
         }
-    }
 
-    // 마지막으로 남아있는 기능 수를 answer에 추가
-    if (count > 0) {
-        answer.push_back(count);
+        answer.push_back(count); // 배포 가능한 기능 개수를 저장
     }
 
     return answer;
