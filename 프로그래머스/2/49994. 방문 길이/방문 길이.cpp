8fc63fe8fbc18f12dbc9386bc260988135b55095
LEE GIWOONG <80272318+gw282@users.noreply.github.com>
2025-04-04 13:17:32 +0900

    [level 2] Title: 방문 길이, Time: 0.12 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -1,49 +1,40 @@
-#include <string>
 #include <iostream>
+#include <string>
+#include <set>
 using namespace std;
 
-bool vis[11][11][11][11] = {false,};
-int x = 5, y = 5;
-
-bool check(int x, int y) {
-    return (x >=0 && x <= 10 && y >= 0 && y<= 10);
-}
-
-
 int solution(string dirs) {
+    set<pair<pair<int,int>, pair<int,int>>> visited;
+    int x = 5, y = 5;
+    int dx[128] = {}, dy[128] = {};
+
+    dx['U'] = 0; dy['U'] = 1;
+    dx['D'] = 0; dy['D'] = -1;
+    dx['L'] = -1; dy['L'] = 0;
+    dx['R'] = 1; dy['R'] = 0;
+
     int answer = 0;
-    
-    for(int i=0;i<dirs.length();i++) {
-        int nx, ny;
-        if(dirs[i] == 'U') {
-            nx = x; ny = y + 1;
-        } else if(dirs[i] == 'D') {
-            nx = x; ny = y - 1;
-        } else if(dirs[i] == 'R') {
-            nx = x + 1; ny = y;
-        } else if(dirs[i] == 'L') {
-            nx = x - 1; ny = y;
+
+    for(char dir : dirs) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if(nx < 0 || nx > 10 || ny < 0 || ny > 10) continue;
+
+        pair<int, int> from = {x, y};
+        pair<int, int> to = {nx, ny};
+
+        // 순서 상관 없이 저장
+        if(visited.find({from, to}) == visited.end() &&
+           visited.find({to, from}) == visited.end()) {
+            visited.insert({from, to});
+            visited.insert({to, from});
+            answer++;
         }
-        if(check(nx,ny)) {
-            if(dirs[i] == 'U' || dirs[i] == 'D') {
-                if(!vis[y][ny][x][x] && !vis[ny][y][x][x]) {
-                    vis[y][ny][x][x] = true;
-                    vis[ny][y][x][x] = true;
-                    answer+=1;
-                    //cout << "y: " << x-5 << ' ' << ny-5 << '\n';
-                }
-                y = ny;
-            } else if(dirs[i] == 'L' || dirs[i] == 'R') {
-                if(!vis[y][y][x][nx] && !vis[y][y][nx][x]) {
-                    vis[y][y][x][nx] = true;
-                    vis[y][y][nx][x] = true;
-                    answer+=1;
-                    //cout << "x: " << nx-5 << ' ' << y-5 << '\n';
-                }
-                x = nx;
-            }
-        }
-        //cout << "po " << x-5 << ' ' << y-5 << '\n';
+
+        x = nx;
+        y = ny;
     }
+
     return answer;
 }
